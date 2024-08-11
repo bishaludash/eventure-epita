4f1d99f3632e2884b163d3921d21ed1140bdc7e4
bishaludash <supertramp.bishal@gmail.com>
2024-08-11 15:24:17 +0200

    add task repository

---
+++ src/main/java/com/bishal/eventure/repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package com.bishal.eventure.repository;
+
+import com.bishal.eventure.model.Task;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface TaskRepository extends MongoRepository<Task, String> {
+    List<Task> findByServerity(int serverity);
+}
