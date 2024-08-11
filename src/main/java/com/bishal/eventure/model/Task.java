0eee3ebf49605844fa0f4f095ab010bb1ca5e7ca
bishaludash <supertramp.bishal@gmail.com>
2024-08-11 15:23:59 +0200

    add task datamodel

---
+++ src/main/java/com/bishal/eventure/model/Task.java
@@ -0,0 +1,21 @@
+package com.bishal.eventure.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection= "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+    @Id
+    private String taskId;
+    private String description;
+    private int serverity;
+    private String assignee;
+    private int storyPoints;
+
+}
